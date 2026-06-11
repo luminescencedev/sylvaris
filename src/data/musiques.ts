2f3d6a4430bff990d9d6fda6635deb914a9a1877
Arthur <arthur.garnier1090@gmail.com>
2026-06-11 23:43:12 +0200

    feat: add new music entry 'HARD!' by Blockba to musiques list

--- src/data/musiques.ts
+++ src/data/musiques.ts
@@ -8,4 +8,5 @@ export interface Musique {
 
 export const musiques: Musique[] = [
   { id: 'garden', youtubeId: 'CV6X8JJrifM', title: 'Garden', artist: 'Blockba' },
+  { id: 'hard', youtubeId: 'qkwxupRpULA', title: 'HARD!', artist: 'Blockba' },
 ]
